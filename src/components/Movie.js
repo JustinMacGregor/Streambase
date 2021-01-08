12f7490acd664a0c125ecea3323e7d5e2ccc5bc2
SriAmin <sri.amin@gmail.com>
2021-01-08 00:31:33 -0500

    Styled to add more movies in a row

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Movie = (props) => {
 
     const api_key = "c4c81ee0662a6bb5b3b0495520d6c51a"
+    
     let streamChoice = []
     async function returnDetails() {
         streamChoice = []
@@ -19,16 +20,17 @@ const Movie = (props) => {
     }
 
     return (
-        <div className="col s12 m6 13">
+        <div className="col s10 m3 13">
             <div className="card">
                 <div className="card-image waves-effect waves-block waves-light">
                     {
-                        props.image == null ? <img src={`https://bit.ly/3956asc`} alt="card image" style={{width:"100%", height:360}}/> : <img src={`http://image.tmdb.org/t/p/w185${props.image}`} alt="card image" style={{width:"100%", height:360}}/>
+                        props.movie.poster_path == null ? <img src={`https://bit.ly/3956asc`} alt="card image" style={{width:"50%", height:420}}/> : <img src={`http://image.tmdb.org/t/p/w185${props.image}`} alt="card image" style={{width:"100%", height:420}}/>
                     }
                 </div>
+                <h5 className="card-title" style={{fontSize:20}}>{props.movie.title}</h5>
                 <div className="card-content">
                     {/* <p><a href="#">View Details</a></p> */}
-                    <p><button onClick={() => returnDetails()}>View Details</button></p>
+                    <p><a href="#" onClick={() => returnDetails()}>View Details</a></p>
                 </div>
             </div>
         </div>
